6a9d23adee0e0aaf5777348537b2c5013940cf24
Kevin <swputer@gmail.com>
2021-09-03 23:04:08 +0900

    feat: 슬랙 slash-command 추가
    - [/점심]으로 검색 시 근처 가게 추천
    - [/점심 ~km] 옵션 작업

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express from 'express';
 import { createServer } from 'http';
+import request from 'request';
 
 import { createEventAdapter } from '@slack/events-api';
 import { WebClient } from '@slack/web-api';
 
-import CONFIG from '../config/bot.json';
+import SLACK_CONFIG from '../config/bot.json';
+import NAVER_CONFIG from '../config/naver.json';
 
 const app = express();
 
-const slackEvents = createEventAdapter(CONFIG.SIGNING_SECRET);
-const webClient = new WebClient(CONFIG.BOT_USER_OAUTH_ACCESS_TOKEN);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Slack Settings
+const slackEvents = createEventAdapter(SLACK_CONFIG.SIGNING_SECRET);
+const webClient = new WebClient(SLACK_CONFIG.BOT_USER_OAUTH_ACCESS_TOKEN);
 
 slackEvents.on('message', async (event) => {
   console.log(event);
@@ -24,10 +30,64 @@ slackEvents.on('message', async (event) => {
 
 app.use('/slack/events', slackEvents.requestListener());
 
-app.use('/slack/slash-command', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write('asd');
-  res.end();
+app.post('/slack/commands', (req, res) => {
+  const option = {
+    query: '선릉 점심',
+    display: 30,
+    start: 1,
+    sort: 'random',
+  };
+
+  request.get(
+    {
+      uri: 'https://openapi.naver.com/v1/search/local',
+      qs: option,
+      headers: {
+        'X-Naver-Client-Id': NAVER_CONFIG.NAVER_CLIENT_ID,
+        'X-Naver-Client-Secret': NAVER_CONFIG.NAVER_CLIENT_SECRET,
+      },
+    },
+    (error, response, body) => {
+      const json = JSON.parse(response.body);
+      const items = json.items;
+      const item = items[Math.floor(Math.random() * items.length)];
+
+      const template = {
+        attachments: [
+          {
+            pretext: '오늘의 점심 메뉴 추천입니다.',
+            color: '#394dd1',
+            fields: [
+              {
+                title: '가게명',
+                value: item.title ? item.title : '없음',
+              },
+              {
+                title: '종류',
+                value: item.category ? item.category : '없음',
+              },
+              {
+                title: '설명',
+                value: item.description ? item.description : '없음',
+              },
+              {
+                title: '전화번호',
+                value: item.telephone ? item.telephone : '없음',
+              },
+              {
+                title: '주소',
+                value: item.address ? item.address : '없음',
+              },
+            ],
+          },
+        ],
+      };
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(template));
+      res.end();
+    }
+  );
 });
 
 createServer(app).listen(3000, () => {
